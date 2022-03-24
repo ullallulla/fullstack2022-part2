e73da2fca12b4f300a10159db9e995d15e1f1050
ullallulla <aleksi.ojansivu@gmail.com>
2022-03-24 12:49:00 +0200

    ex 2.5

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,40 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ parts }) => {
-
-  const total = parts.reduce((sum, part) => {
-    return sum + part.exercises
-  }, 0)
-
-  return (
-    <p><b>total of {total} exercises</b></p>
-  )
-}
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    <Part
-      part={parts}
-    />
-  </>
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      {course.parts.map(part =>
-        <Content key={part.id} parts={part} />
-      )}
-      <Total parts={course.parts} />
-
-    </>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
