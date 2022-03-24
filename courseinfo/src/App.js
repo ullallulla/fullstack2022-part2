e4797dbcc3bdf5e91313a723c18154b601138958
ullallulla <aleksi.ojansivu@gmail.com>
2022-03-24 11:57:02 +0200

    ex 2.1

---
+++ courseinfo/src/App.js
@@ -0,0 +1,55 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    <Part
+      part={parts} 
+    />    
+  </>
+
+const Course = ({course}) => {
+  return(
+  <>
+    <Header course={course.name}/>
+    {course.parts.map(part =>
+      <Content key={part.id} parts={part}/>
+    )}
+    
+  </>
+  )}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+export default App
