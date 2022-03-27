08a1aff6be4972017aa5a3c30f4795e1b600e98f
ullallulla <aleksi.ojansivu@gmail.com>
2022-03-27 13:52:14 +0300

    ex 2.16

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/personService'
 
 const Filter = ({ filter, handleFilter }) => {
   return (
@@ -44,12 +45,10 @@ const App = () => {
   const [newFilter, setNewFilter] = useState('')
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        console.log('promise fulfilled')
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
   }, [])
 
@@ -63,10 +62,10 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`)
     }
     else {
-      axios
-        .post('http://localhost:3001/persons', personObject)
-        .then(response => {
-          setPersons(persons.concat(response.data))
+      personService
+        .create(personObject)
+        .then(returnedPerson => {
+          setPersons(persons.concat(returnedPerson))
           setNewName('')
           setNewNumber('')
         })
