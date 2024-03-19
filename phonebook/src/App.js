0afb80eb04ca157e498e32d82cc6afc5bd380a22
ullallulla <aleksi.ojansivu@gmail.com>
2024-03-19 15:26:00 +0200

    added timeout for error message

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -71,6 +71,9 @@ const App = () => {
         })
         .catch(error => {
           setErrorMessage(`${error.response.data.error}`)
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
         })
     }
   }
